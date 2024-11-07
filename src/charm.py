a97761e6bc7a203f63273595b7e2bd7888c31e2b
reneradoi <rene.radoi@canonical.com>
2024-11-07 10:42:38 +0000

    `data-platform-workflows` require metadata.yaml

--- src/charm.py
+++ src/charm.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Charm the application."""
+"""Charmed machine operator for etcd."""
 
 import logging
 
@@ -16,10 +17,20 @@ class EtcdOperatorCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on.start, self._on_start)
+        framework.observe(self.on.install, self._on_install)
+        framework.observe(self.on.config_changed, self._on_config_changed)
 
-    def _on_start(self, event: ops.StartEvent):
+    def _on_start(self, event: ops.StartEvent) -> None:
         """Handle start event."""
         self.unit.status = ops.ActiveStatus()
+
+    def _on_install(self, event: ops.InstallEvent) -> None:
+        """Handle install event."""
+        pass
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
+        """Handle config_changed event."""
+        pass
 
 
 if __name__ == "__main__":  # pragma: nocover
