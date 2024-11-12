bf1c3a29246eba2275313126751fc27f7c7e7c40
reneradoi <rene.radoi@canonical.com>
2024-11-12 17:31:30 +0000

    move base events from charm.py to `EtcdEvents` class

--- src/charm.py
+++ src/charm.py
@@ -8,16 +8,10 @@ import logging
 
 import ops
 from ops import StatusBase
-from ops.charm import (
-    LeaderElectedEvent,
-    RelationChangedEvent,
-    RelationCreatedEvent,
-    RelationDepartedEvent,
-    RelationJoinedEvent,
-)
 
 from core.cluster import ClusterState
-from literals import PEER_RELATION, SUBSTRATE, DebugLevel, Status
+from events.etcd import EtcdEvents
+from literals import SUBSTRATE, DebugLevel, Status
 from managers.cluster import ClusterManager
 from workload import EtcdWorkload
 
@@ -35,63 +29,10 @@ class EtcdOperatorCharm(ops.CharmBase):
         # --- MANAGERS ---
         self.cluster_manager = ClusterManager
 
-        # --- CORE EVENTS ---
-        self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.start, self._on_start)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(
-            self.on[PEER_RELATION].relation_created, self._on_cluster_relation_created
-        )
-        self.framework.observe(
-            self.on[PEER_RELATION].relation_joined, self._on_cluster_relation_joined
-        )
-        self.framework.observe(
-            self.on[PEER_RELATION].relation_changed, self._on_cluster_relation_changed
-        )
-        self.framework.observe(
-            self.on[PEER_RELATION].relation_departed, self._on_cluster_relation_departed
-        )
-        self.framework.observe(self.on.leader_elected, self._on_leader_elected)
+        # --- EVENT HANDLERS ---
+        self.etcd_events = EtcdEvents(self)
 
-    def _on_install(self, event: ops.InstallEvent) -> None:
-        """Handle install event."""
-        install = self.workload.install()
-        if not install:
-            self._set_status(Status.SERVICE_NOT_INSTALLED)
-            event.defer()
-            return
-
-    def _on_start(self, event: ops.StartEvent) -> None:
-        """Handle start event."""
-        self._set_status(Status.ACTIVE)
-
-    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
-        """Handle config_changed event."""
-        pass
-
-    def _on_cluster_relation_created(self, event: RelationCreatedEvent) -> None:
-        """Handle event received by a new unit when joining the cluster relation."""
-        pass
-
-    def _on_cluster_relation_changed(self, event: RelationChangedEvent) -> None:
-        """Handle all events related to the cluster-peer relation."""
-        self.state.unit_server.update(self.cluster_manager.get_host_mapping())
-
-    def _on_cluster_relation_departed(self, event: RelationDepartedEvent) -> None:
-        """Handle event received by a unit leaves the cluster relation."""
-        pass
-
-    def _on_cluster_relation_joined(self, event: RelationJoinedEvent) -> None:
-        """Handle event received by all units when a new unit joins the cluster relation."""
-        pass
-
-    def _on_leader_elected(self, event: LeaderElectedEvent) -> None:
-        """Handle all events in the 'cluster' peer relation."""
-        if not self.state.peer_relation:
-            self._set_status(Status.NO_PEER_RELATION)
-            return
-
-    def _set_status(self, key: Status) -> None:
+    def set_status(self, key: Status) -> None:
         """Set charm status."""
         status: StatusBase = key.value.status
         log_level: DebugLevel = key.value.log_level
