6e3879b0ea7f0dc93725afd2546a256733b75f37
reneradoi <rene.radoi@canonical.com>
2024-11-25 14:07:28 +0000

    doc string comment

--- src/managers/cluster.py
+++ src/managers/cluster.py
@@ -91,6 +91,10 @@ class EtcdClient:
     ) -> str:
         """Execute `etcdctl` command via subprocess.
 
+        The list of arguments will be extended once authentication/encryption is implemented.
+        This method aims to provide a very clear interface for executing `etcdctl` and minimize
+        the margin of error on cluster operations.
+
         Args:
             command: command to execute with etcdctl, e.g. `elect`, `member` or `endpoint`
             subcommand: subcommand to add to the previous command, e.g. `add` or `status`
@@ -99,7 +103,10 @@ class EtcdClient:
             ...
 
         Returns:
-            The output of the subprocess-command as a string.
+            The output of the subprocess-command as a string. In case of error, this will
+            return an empty string. It will not raise an error in order to leave error handling
+            up to the caller. Depending on what command is executed, the ways of handling errors
+            might differ.
         """
         try:
             result = subprocess.run(
