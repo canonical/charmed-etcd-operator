e8eb8a92ca840831a60209bb789da5418d5861cd
reneradoi <rene.radoi@canonical.com>
2025-01-06 16:52:29 +0000

    integration test: check cluster formation after scale up

--- tests/integration/ha/test_scaling.py
+++ tests/integration/ha/test_scaling.py
@@ -7,7 +7,12 @@ import logging
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from ..helpers import APP_NAME
+from ..helpers import (
+    APP_NAME,
+    get_cluster_endpoints,
+    get_cluster_members,
+    get_juju_leader_unit_name,
+)
 from .helpers import existing_app
 
 logger = logging.getLogger(__name__)
@@ -37,6 +42,7 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
 async def test_scale_up(ops_test: OpsTest) -> None:
     """Make sure new units are added to the etcd cluster."""
     app = (await existing_app(ops_test)) or APP_NAME
+    model = ops_test.model_full_name
     init_units_count = len(ops_test.model.applications[app].units)
 
     # scale up
@@ -49,3 +55,10 @@ async def test_scale_up(ops_test: OpsTest) -> None:
     )
     num_units = len(ops_test.model.applications[app].units)
     assert num_units == init_units_count + 2
+
+    # check if all units have been added to the cluster
+    endpoints = get_cluster_endpoints(ops_test, APP_NAME)
+    leader_unit = await get_juju_leader_unit_name(ops_test, APP_NAME)
+
+    cluster_members = get_cluster_members(model, leader_unit, endpoints)
+    assert len(cluster_members) == init_units_count + 2
