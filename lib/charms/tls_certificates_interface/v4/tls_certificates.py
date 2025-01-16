a5f7988e11de4a148934f6667141eeeafb1b510a
Smail Kourta <smail.kourta@canonical.com>
2025-01-16 12:22:04 +0000

    updated tls_certificates library

--- lib/charms/tls_certificates_interface/v4/tls_certificates.py
+++ lib/charms/tls_certificates_interface/v4/tls_certificates.py
@@ -52,7 +52,7 @@ LIBAPI = 4
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 5
 
 PYDEPS = ["cryptography", "pydantic"]
 
@@ -1025,12 +1025,19 @@ class TLSCertificatesRequiresV4(Object):
         self._find_available_certificates()
         self._cleanup_certificate_requests()
 
-    def _mode_is_valid(self, mode) -> bool:
+    def _mode_is_valid(self, mode: Mode) -> bool:
         return mode in [Mode.UNIT, Mode.APP]
 
     def _on_secret_remove(self, event: SecretRemoveEvent) -> None:
         """Handle Secret Removed Event."""
-        event.secret.remove_revision(event.revision)
+        try:
+            event.secret.remove_revision(event.revision)
+        except SecretNotFoundError:
+            logger.warning(
+                "No such secret %s, nothing to remove",
+                event.secret.label or event.secret.id,
+            )
+            return
 
     def _on_secret_expired(self, event: SecretExpiredEvent) -> None:
         """Handle Secret Expired Event.
