6afe9846f8c2c0d5c687e322d66bb1ee835500bf
Smail Kourta <smail.kourta@canonical.com>
2025-01-24 11:08:07 +0000

    add marks to the set private key test

--- tests/integration/tls/test_private_key_rotation.py
+++ tests/integration/tls/test_private_key_rotation.py
@@ -109,6 +109,9 @@ async def test_tls_enabled(ops_test: OpsTest) -> None:
     ), "Failed to read key"
 
 
+@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
 async def test_set_private_key(ops_test: OpsTest) -> None:
     """Set a new private key and check if the cluster is still accessible."""
     model = ops_test.model_full_name
