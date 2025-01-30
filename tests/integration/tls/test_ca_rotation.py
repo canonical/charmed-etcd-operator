90548841371f2a2e63049e6d180be11656e6a7af
René Radoi <rene.radoi@canonical.com>
2025-01-30 17:47:41 +0000

    execute tests on "noble" runners

--- tests/integration/tls/test_ca_rotation.py
+++ tests/integration/tls/test_ca_rotation.py
@@ -33,7 +33,7 @@ TEST_VALUE = "42"
 CERTIFICATE_EXPIRY_TIME = 90
 
 
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+@pytest.mark.runner(["self-hosted", "linux", "X64", "noble", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy_with_tls(ops_test: OpsTest) -> None:
@@ -91,7 +91,7 @@ async def test_build_and_deploy_with_tls(ops_test: OpsTest) -> None:
     ), "Failed to read key"
 
 
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+@pytest.mark.runner(["self-hosted", "linux", "X64", "noble", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_ca_rotation(ops_test: OpsTest) -> None:
