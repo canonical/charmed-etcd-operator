bfbb684e41dc68486f6939d47c326c93b260f442
René Radoi <rene.radoi@canonical.com>
2025-02-06 16:54:04 +0000

    enhance integration test

--- tests/integration/ha/test_storage_reuse.py
+++ tests/integration/ha/test_storage_reuse.py
@@ -38,19 +38,15 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
 
     assert len(ops_test.model.applications[APP_NAME].units) == NUM_UNITS
 
+
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 async def test_attach_storage_after_scale_down(ops_test: OpsTest) -> None:
-    """Make sure a unit is removed from the etcd cluster without downtime."""
+    """Make sure storage can be re-attached after removing a unit."""
     app = (await existing_app(ops_test)) or APP_NAME
     unit = ops_test.model.applications[app].units[-1]
     storage_id = get_storage_id(ops_test, unit.name, "data")
-
-    # create a testfile to check if data in storage is persistent
-    testfile = "/var/snap/charmed-etcd/common/var/lib/etcd/testfile"
-    create_testfile_cmd = f"juju ssh {unit.name} --model={ops_test.model.info.name} -q sudo touch {testfile}"
-    subprocess.run(create_testfile_cmd, shell=True)
 
     # remove the unit
     await ops_test.model.applications[app].destroy_unit(unit.name)
@@ -65,7 +61,9 @@ async def test_attach_storage_after_scale_down(ops_test: OpsTest) -> None:
     )
 
     # add unit with previous storage attached
-    add_unit_cmd = f"add-unit {app} --model={ops_test.model.info.name} --attach-storage={storage_id}"
+    add_unit_cmd = (
+        f"add-unit {app} --model={ops_test.model.info.name} --attach-storage={storage_id}"
+    )
     return_code, _, _ = await ops_test.juju(*add_unit_cmd.split())
     assert return_code == 0, f"Failed to add unit with storage {storage_id}"
 
@@ -80,6 +78,68 @@ async def test_attach_storage_after_scale_down(ops_test: OpsTest) -> None:
         timeout=1000,
     )
 
-    # check if the testfile is still there
-    check_testfile_cmd = f"juju ssh {new_unit.name} --model={ops_test.model.info.name} -q sudo ls {testfile}"
-    assert testfile == subprocess.getoutput(check_testfile_cmd)
+    # to do: check cluster membership
+    # to do: test data on the new unit and the cluster
+
+
+@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_attach_storage_after_scale_to_zero(ops_test: OpsTest) -> None:
+    """Make sure storage can be re-attached after removing all units."""
+    app = (await existing_app(ops_test)) or APP_NAME
+
+    # remove all units except one - we need to know which storage to attach when scaling up again
+    for unit in ops_test.model.applications[app].units[1:]:
+        await ops_test.model.applications[app].destroy_unit(unit.name)
+
+    await ops_test.model.wait_for_idle(
+        apps=[app],
+        status="active",
+        wait_for_exact_units=1,
+        wait_for_active=True,
+        # if the cluster member cannot be removed immediately, the `storage_detaching` hook might fail temporarily
+        raise_on_error=False,
+        timeout=1000,
+    )
+
+    # remove the remaining unit after saving the storage id
+    unit = ops_test.model.applications[app].units[0]
+    storage_id = get_storage_id(ops_test, unit.name, "data")
+    await ops_test.model.applications[app].destroy_unit(unit.name)
+
+    await ops_test.model.wait_for_idle(
+        apps=[app],
+        wait_for_exact_units=0,
+        # if the cluster member cannot be removed immediately, the `storage_detaching` hook might fail temporarily
+        raise_on_error=False,
+        timeout=1000,
+    )
+
+    # scale up again re-attaching the storage
+    add_unit_cmd = (
+        f"add-unit {app} --model={ops_test.model.info.name} --attach-storage={storage_id}"
+    )
+    return_code, _, _ = await ops_test.juju(*add_unit_cmd.split())
+    assert return_code == 0, f"Failed to add unit with storage {storage_id}"
+
+    await ops_test.model.wait_for_idle(
+        apps=[app],
+        status="active",
+        wait_for_exact_units=1,
+        wait_for_active=True,
+        timeout=1000,
+    )
+
+    # to do: check cluster membership
+    # to do: test data on the new unit and the cluster
+
+    # scale up
+    await ops_test.model.applications[app].add_unit(count=2)
+    await ops_test.model.wait_for_idle(
+        apps=[app],
+        status="active",
+        wait_for_exact_units=NUM_UNITS,
+        wait_for_active=True,
+        timeout=1000,
+    )
