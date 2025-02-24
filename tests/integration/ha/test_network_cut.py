9a99374b377d264308fe29ae6bf9dcc93ce8cc7c
René Radoi <rene.radoi@canonical.com>
2025-02-24 16:08:52 +0000

    run all the integration tests for network cut

--- tests/integration/ha/test_network_cut.py
+++ tests/integration/ha/test_network_cut.py
@@ -61,7 +61,6 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
     await wait_until(ops_test, apps=[APP_NAME], timeout=1000)
 
 
-@pytest.mark.skip()
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
