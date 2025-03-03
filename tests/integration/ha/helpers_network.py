71fe6e70942408721ee3b3936a99da0e7f4c9d57
René Radoi <rene.radoi@canonical.com>
2025-03-03 09:27:14 +0000

    do not set network device `limits.priority` when throttling network, can cause issues with github runners

--- tests/integration/ha/helpers_network.py
+++ tests/integration/ha/helpers_network.py
@@ -80,8 +80,6 @@ def cut_network_from_unit_without_ip_change(machine_name: str) -> None:
     subprocess.check_call(limit_set_command.split())
     limit_set_command = f"lxc config device set {machine_name} eth0 limits.ingress=1kbit"
     subprocess.check_call(limit_set_command.split())
-    limit_set_command = f"lxc config device set {machine_name} eth0 limits.priority=10"
-    subprocess.check_call(limit_set_command.split())
 
 
 def restore_network_for_unit_with_ip_change(machine_name: str) -> None:
