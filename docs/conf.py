3deeea08a5c74e609893ab022b8e657671ba87cf
Andreia <andreia.velasco@canonical.com>
2025-04-14 10:16:28 +0200

    Fix syntax error

--- docs/conf.py
+++ docs/conf.py
@@ -194,7 +194,7 @@ linkcheck_ignore = [
 
 linkcheck_anchors_ignore_for_url = [
     r"https://github\.com/.*",
-    r"https://canonical-juju.readthedocs-hosted.com/en/latest/*
+    r"https://canonical-juju.readthedocs-hosted.com/en/latest/*"
     ]
 
 # give linkcheck multiple tries on failure
