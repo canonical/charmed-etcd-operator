c09c98a7779d7b743bdf15a0aed0e07c0996c7e3
reneradoi <rene.radoi@canonical.com>
2025-06-02 19:36:32 +0200

    small fix

--- src/literals.py
+++ src/literals.py
@@ -84,7 +84,7 @@ class Status(Enum):
     HEALTH_CHECK_FAILED = StatusLevel(MaintenanceStatus("health check failed"), "DEBUG")
     NO_PEER_RELATION = StatusLevel(MaintenanceStatus("no peer relation available"), "DEBUG")
     OBJECT_STORAGE_CONFLICT = StatusLevel(
-        BlockedStatus("Azure and S3 storage configured - please remove one"), "ERROR"
+        BlockedStatus("Azure and S3 storages configured - please remove one"), "ERROR"
     )
     PASSWORD_UPDATE_FAILED = StatusLevel(BlockedStatus("failed to update password"), "ERROR")
     PEER_URL_NOT_SET = StatusLevel(MaintenanceStatus("peer-url not set"), "DEBUG")
