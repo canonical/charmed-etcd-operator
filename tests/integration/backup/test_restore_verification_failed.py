5d2f4da2ef1cc6edbc7524b73c8c541dac3a4c89
reneradoi <rene.radoi@canonical.com>
2025-06-03 15:05:18 +0200

    move to separate integration test file

--- tests/integration/backup/test_restore_verification_failed.py
+++ tests/integration/backup/test_restore_verification_failed.py
@@ -51,10 +51,6 @@ async def test_deploy_and_configure(
     await set_credentials_action.wait()
     await wait_until(ops_test, apps=[APP_NAME, S3_INTEGRATOR], apps_statuses=["active"])
 
-    logger.info("Configure admin credentials in etcd")
-    await set_password(ops_test, PASSWORD)
-    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS)
-
 
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy"])
 @pytest.mark.group(1)
@@ -124,8 +120,12 @@ async def test_create_backup(ops_test: OpsTest) -> None:
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
-async def test_restore_backup_on_same_cluster(ops_test: OpsTest) -> None:
-    """Restore a backup and check if data is recovered."""
+async def test_restore_verification_failed(ops_test: OpsTest):
+    """Restore a backup with invalid admin password."""
+    logger.info("Configure admin credentials in etcd")
+    await set_password(ops_test, "invalid_password")
+    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS)
+
     for unit in ops_test.model.applications[APP_NAME].units:
         if await unit.is_leader_from_status():
             leader_unit = unit
@@ -134,54 +134,16 @@ async def test_restore_backup_on_same_cluster(ops_test: OpsTest) -> None:
     logger.info(f"Restoring backup {backup_id}")
     restore_action = await leader_unit.run_action("restore", **{"backup-id": backup_id})
     restore_backup_response = await restore_action.wait()
-    assert restore_backup_response.results.get("return-code") == 0, "restore failed"
+    assert restore_backup_response.results.get("return-code") == 0, "restore action failed"
 
-    # wait for the restore to be performed across all units and check the restored data
-    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS)
-
-    endpoints = get_cluster_endpoints(ops_test, APP_NAME)
-    assert get_key(endpoints, user=INTERNAL_USER, password=PASSWORD, key=TEST_KEY) == TEST_VALUE, (
-        "data not recovered"
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_restore_backup_on_different_cluster(ops_test: OpsTest):
-    """Restore a backup and check if data is recovered."""
-    logger.info("Remove existing etcd cluster and deploy a new one.")
-    await ops_test.model.remove_application(APP_NAME, block_until_done=True)
-    await ops_test.model.deploy(CHARM_PATH, num_units=NUM_UNITS)
-    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS, idle_period=60)
-
-    logger.info("Configure admin credentials in etcd")
-    await set_password(ops_test, PASSWORD)
-    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS)
-
-    logger.info(f"Integrate the newly deployed application with {S3_INTEGRATOR}")
-    await ops_test.model.integrate(APP_NAME, S3_INTEGRATOR)
     await wait_until(
         ops_test,
-        apps=[APP_NAME, S3_INTEGRATOR],
-        apps_statuses=["active"],
-        units_statuses=["active"],
-    )
-
-    for unit in ops_test.model.applications[APP_NAME].units:
-        if await unit.is_leader_from_status():
-            leader_unit = unit
-
-    # download the backup from storage and restore it
-    logger.info(f"Restoring backup {backup_id}")
-    restore_action = await leader_unit.run_action("restore", **{"backup-id": backup_id})
-    restore_backup_response = await restore_action.wait()
-    assert restore_backup_response.results.get("return-code") == 0, "restore failed"
-
-    # wait for the restore to be performed across all units and check the restored data
-    await wait_until(ops_test, apps=[APP_NAME], wait_for_exact_units=NUM_UNITS)
-
-    endpoints = get_cluster_endpoints(ops_test, APP_NAME)
-    assert get_key(endpoints, user=INTERNAL_USER, password=PASSWORD, key=TEST_KEY) == TEST_VALUE, (
-        "data not recovered"
+        apps=[APP_NAME],
+        units_full_statuses={
+            APP_NAME: {
+                "units": {
+                    "blocked": [Status.RESTORE_VERIFICATION_FAILED.value.status.message],
+                }
+            },
+        },
     )
