981dfc3171a26e74f27704b7abf71ba9dcef06d9
Mykola Marzhan <mykola.marzhan@canonical.com>
2025-07-18 20:07:00 +0200

    fix mocking

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,7 +2,19 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent.parent / "src"))
+sys.path.append(str(Path(__file__).parent.parent.parent / "lib"))
+
 import pytest
+import pathlib
+import shutil
+import tomli
+import tomli_w
+import platform
+
 from ops import testing
 
 from charm import EtcdOperatorCharm
@@ -108,3 +120,44 @@ def cluster_no_tls_context():
     restart_relation = testing.PeerRelation(id=4, endpoint="restart")
 
     return ctx, [peer_relation, restart_relation]
+
+class _MockRefresh:
+    in_progress = False
+    next_unit_allowed_to_refresh = True
+    workload_allowed_to_start = True
+    app_status_higher_priority = None
+    unit_status_higher_priority = None
+
+    def __init__(self, _, /):
+        pass
+
+    def update_snap_revision(self):
+        pass
+
+    @property
+    def pinned_snap_revision(self):
+        with pathlib.Path("refresh_versions.toml").open("rb") as file:
+            return tomli.load(file)["snap"]["revisions"][platform.machine()]
+
+    def unit_status_lower_priority(self, *, workload_is_running=True):
+        return None
+
+
+@pytest.fixture(autouse=True)
+def patch(monkeypatch):
+    monkeypatch.setattr("charm_refresh.Machines", _MockRefresh)
+
+    # Add charm version to refresh_versions.toml
+    path = pathlib.Path("refresh_versions.toml")
+    backup = pathlib.Path("refresh_versions.toml.backup")
+    shutil.copy(path, backup)
+    with path.open("rb") as file:
+        versions = tomli.load(file)
+    versions["charm"] = "16/0.0.0"
+    with path.open("wb") as file:
+        tomli_w.dump(versions, file)
+
+    yield
+
+    path.unlink()
+    shutil.move(backup, path)
