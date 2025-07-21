0d2142a97476122de43581caa5ff0f87df424e53
reneradoi <rene.radoi@canonical.com>
2025-07-21 15:13:24 +0200

    issue `member_list()` and `broadcast_peer_url` commands against own cluster endpoint only

--- src/managers/cluster.py
+++ src/managers/cluster.py
@@ -121,7 +121,7 @@ class ClusterManager:
         client = EtcdClient(
             username=self.admin_user,
             password=self.admin_password,
-            client_url=",".join(e for e in self.cluster_endpoints),
+            client_url=self.state.unit_server.client_url,
         )
 
         member_list = client.member_list()
@@ -150,7 +150,7 @@ class ClusterManager:
         client = EtcdClient(
             username=self.admin_user,
             password=self.admin_password,
-            client_url=",".join(e for e in self.cluster_endpoints),
+            client_url=self.state.unit_server.client_url,
         )
         client.broadcast_peer_url(self.member.id, peer_urls)
 
