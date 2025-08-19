364bfa0342e4b37f7b112a5392c16e5a8171ad7f
René Radoi <rene.radoi@canonical.com>
2025-08-19 08:36:36 +0200

    [DPE-7424] Update Python 3.12 and ops 3 (#143)
    This PR adds the Python version used by charmed etcd, as well as a few
    dependencies such as `ops`.

--- src/literals.py
+++ src/literals.py
@@ -5,7 +5,7 @@
 """Collection of global literals for the etcd charm."""
 
 from dataclasses import dataclass
-from enum import Enum
+from enum import StrEnum
 from typing import Literal
 
 from ops.model import StatusBase
@@ -60,7 +60,7 @@ class StatusLevel:
     log_level: DebugLevel
 
 
-class EtcdClusterState(Enum):
+class EtcdClusterState(StrEnum):
     """Enum for Cluster state in etcd."""
 
     EXISTING = "existing"
@@ -68,7 +68,7 @@ class EtcdClusterState(Enum):
 
 
 # enum for TLS state
-class TLSState(Enum):
+class TLSState(StrEnum):
     """Enum for TLS state."""
 
     NO_TLS = "no-tls"
@@ -77,14 +77,14 @@ class TLSState(Enum):
     TO_NO_TLS = "to-no-tls"
 
 
-class TLSType(Enum):
+class TLSType(StrEnum):
     """TLS types."""
 
     PEER = "peer"
     CLIENT = "client"
 
 
-class TLSCARotationState(Enum):
+class TLSCARotationState(StrEnum):
     """TLS CA Rotation state."""
 
     NO_ROTATION = "no-rotation"
@@ -94,7 +94,7 @@ class TLSCARotationState(Enum):
 
 
 # enum for Backup state
-class RestoreStep(Enum):
+class RestoreStep(StrEnum):
     """Backup / Restore workflow step representation."""
 
     NOT_STARTED = ""
@@ -106,7 +106,7 @@ class RestoreStep(Enum):
     COMPLETED = "completed"
 
 
-class TuningOptions(Enum):
+class TuningOptions(StrEnum):
     """Configuration options for tuning etcd performance."""
 
     ELECTION_TIMEOUT_CONFIG = "election-timeout"
