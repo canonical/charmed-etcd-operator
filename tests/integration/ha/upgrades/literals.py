2d8de08a90087a4db1222d2d0009090d8b93a8ca
René Radoi <rene.radoi@canonical.com>
2025-10-07 12:35:23 +0200

    [DPE-8407] update etcd to version 3.6.5 (#158)
    ## Description of issue or feature:
    This PR updates etcd to version 3.6.5. Release notes for the upstream
    release can be found
    [here](https://github.com/etcd-io/etcd/blob/main/CHANGELOG/CHANGELOG-3.6.md#v365-2025-09-19).
    
    https://warthogs.atlassian.net/browse/DPE-8407
    
    ## Solution:
    - update the snap revisions to deploy in `refresh_versions.toml`
    - update the integration test config for upgrades
    
    ## How was this change tested?
    - [ ] Manually
    - [ ] Unit tests
    - [X] Integration tests

--- tests/integration/ha/upgrades/literals.py
+++ tests/integration/ha/upgrades/literals.py
@@ -6,6 +6,6 @@
 
 NUM_UNITS = 3
 CHARM_CHANNEL = "3.6/edge"
-CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 94, "aarch64": 95}
-WORKLOAD_VERSION = {"previous": "3.6.2", "target": "3.6.4"}
+CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 102, "aarch64": 103}
+WORKLOAD_VERSION = {"previous": "3.6.4", "target": "3.6.5"}
 CERTIFICATE_EXPIRY_TIME = 250
