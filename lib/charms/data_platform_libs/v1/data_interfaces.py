6189651d329138c5637d97697355ef4c88c042ee
Smail Kourta <smail.kourta@canonical.com>
2025-10-13 14:50:11 +0400

    update v1 lib

--- lib/charms/data_platform_libs/v1/data_interfaces.py
+++ lib/charms/data_platform_libs/v1/data_interfaces.py
@@ -212,7 +212,7 @@ class SampleCharm(CharmBase):
         # Database generic helper
         self.database = DatabaseHelper()
 
-    def _on_database_requested(self, event: ResourceRequestedEvent) -> None:
+    def _on_resource_requested(self, event: ResourceRequestedEvent) -> None:
         # Handle the event triggered by a new database requested in the relation
         # Retrieve the database name using the charm library.
         db_name = event.request.resource
@@ -231,7 +231,7 @@ class SampleCharm(CharmBase):
         self.provided_database.set_response(event.relation.id, response)
 ```
 
-As shown above, the library provides a custom event (database_requested) to handle
+As shown above, the library provides a custom event (resource_requested) to handle
 the situation when an application charm requests a new database to be created.
 It's preferred to subscribe to this event instead of relation changed event to avoid
 creating a new database when other information other than a database name is
@@ -247,7 +247,6 @@ import pickle
 import random
 import string
 from abc import ABC, abstractmethod
-from collections import namedtuple
 from datetime import datetime
 from enum import Enum
 from typing import (
@@ -256,6 +255,7 @@ from typing import (
     ClassVar,
     Generic,
     Literal,
+    NamedTuple,
     NewType,
     TypeAlias,
     TypeVar,
@@ -275,7 +275,7 @@ from ops import (
     SecretInfo,
     SecretNotFoundError,
 )
-from ops.charm import CharmEvents
+from ops.charm import CharmEvents, SecretRemoveEvent
 from ops.framework import EventSource, Handle, Object
 from ops.model import Application, ModelError, Relation, Unit
 from pydantic import (
@@ -321,6 +321,7 @@ MODEL_ERRORS = {
     "not_leader": "this unit is not the leader",
     "no_label_and_uri": "ERROR either URI or label should be used for getting an owned secret but not both",
     "owner_no_refresh": "ERROR secret owner cannot use --refresh",
+    "permission_denied": "ERROR permission denied",
 }
 
 RESOURCE_ALIASES = [
@@ -399,13 +400,17 @@ def get_encoded_dict(
     logger.error("Unexpected datatype for %s instead of dict.", str(data))
 
 
-Diff = namedtuple("Diff", ["added", "changed", "deleted"])
-Diff.__doc__ = """
-A tuple for storing the diff between two data mappings.
+class Diff(NamedTuple):
+    """A tuple for storing the diff between two data mappings.
 
-added - keys that were added
-changed - keys that still exist but have new values
-deleted - key that were deleted"""
+    added - keys that were added
+    changed - keys that still exist but have new values
+    deleted - key that were deleted
+    """
+
+    added: set[str]
+    changed: set[str]
+    deleted: set[str]
 
 
 def diff(old_data: dict[str, str] | None, new_data: dict[str, str]) -> Diff:
@@ -443,7 +448,14 @@ def store_new_data(
     new_data: dict[str, str],
     short_uuid: str | None = None,
 ):
-    """Stores the new data in the databag for diff computation."""
+    """Stores the new data in the databag for diff computation.
+
+    Args:
+        relation: The relation considered to write data to
+        component: The component databag to write data to
+        new_data: a dictionary containing the data to write
+        short_uuid: Only present in V1, the request-id of that data to write.
+    """
     # First, the case for V0
     if not short_uuid:
         relation.data[component].update({"data": json.dumps(new_data)})
@@ -452,9 +464,8 @@ def store_new_data(
         data = json.loads(relation.data[component].get("data", "{}"))
         if not isinstance(data, dict):
             raise ValueError
-        newest_data = copy.deepcopy(data)
-        newest_data[short_uuid] = new_data
-        relation.data[component].update({"data": json.dumps(newest_data)})
+        data[short_uuid] = new_data
+        relation.data[component].update({"data": json.dumps(data)})
 
 
 ##############################################################################
@@ -503,7 +514,11 @@ class CachedSecret:
     The data structure is precisely reusing/simulating as in the actual Secret Storage
     """
 
-    KNOWN_MODEL_ERRORS = [MODEL_ERRORS["no_label_and_uri"], MODEL_ERRORS["owner_no_refresh"]]
+    KNOWN_MODEL_ERRORS = [
+        MODEL_ERRORS["no_label_and_uri"],
+        MODEL_ERRORS["owner_no_refresh"],
+        MODEL_ERRORS["permission_denied"],
+    ]
 
     def __init__(
         self,
@@ -1142,8 +1157,6 @@ class OpsRepository(AbstractRepository):
 
     SECRET_FIELD_NAME: str
 
-    IGNORES_GROUPS: list[SecretGroup] = []
-
     uri_to_databag: bool = True
 
     def __init__(
@@ -1353,9 +1366,6 @@ class OpsRepository(AbstractRepository):
         if self.component not in self.relation.data:
             logger.info(f"Component {self.component} not in relation {self.relation}")
             return None
-        if secret_group in self.IGNORES_GROUPS:
-            logger.warning(f"Trying to get invalid secret group {secret_group}")
-            return None
 
         label = self._generate_secret_label(self.relation, secret_group, short_uuid=short_uuid)
 
@@ -1368,17 +1378,13 @@ class OpsRepository(AbstractRepository):
         secret_group: SecretGroup,
         uri: str | None = None,
         short_uuid: str | None = None,
-    ) -> Any | None:
+    ) -> str | None:
         """Gets a value for a field stored in a secret group."""
         if not self.relation:
             logger.info("No relation to get value from")
             return None
         if self.component not in self.relation.data:
             logger.info(f"Component {self.component} not in relation {self.relation}")
-            return None
-
-        if secret_group in self.IGNORES_GROUPS:
-            logger.warning(f"Trying to get invalid secret group {secret_group}")
             return None
 
         secret_field = self.secret_field(secret_group, field)
@@ -1423,9 +1429,6 @@ class OpsRepository(AbstractRepository):
         if self.component not in self.relation.data:
             logger.info(f"Component {self.component} not in relation {self.relation}")
             return None
-
-        if secret_group in self.IGNORES_GROUPS:
-            logger.warning(f"Trying to get invalid secret group {secret_group}")
 
         label = self._generate_secret_label(self.relation, secret_group, short_uuid)
 
@@ -1472,13 +1475,6 @@ class OpsPeerRepository(OpsRepository):
     """Implementation of the Ops Repository for peer relations."""
 
     SECRET_FIELD_NAME = "internal_secret"
-
-    IGNORES_GROUPS = [
-        SecretGroup("user"),
-        SecretGroup("entity"),
-        SecretGroup("mtls"),
-        SecretGroup("tls"),
-    ]
 
     uri_to_databag: bool = False
 
@@ -2039,6 +2035,7 @@ class EventHandlers(Object):
             charm.on.secret_changed,
             self._on_secret_changed_event,
         )
+        self.framework.observe(charm.on.secret_remove, self._on_secret_remove_event)
 
     @property
     def relations(self) -> list[Relation]:
@@ -2069,6 +2066,34 @@ class EventHandlers(Object):
     def _on_secret_changed_event(self, event: SecretChangedEvent) -> None:
         """Event emitted when the relation data has changed."""
         raise NotImplementedError
+
+    def _on_secret_remove_event(self, event: SecretRemoveEvent) -> None:
+        """Event emitted when a secret is removed.
+
+        A secret removal (entire removal, not just a revision removal) causes
+        https://github.com/juju/juju/issues/20794. This check is to avoid the
+        errors that would happen if we tried to remove the revision in that case
+        (in the revision removal, the label is present).
+        """
+        if not event.secret.label:
+            return
+        relation = self._relation_from_secret_label(event.secret.label)
+
+        if not relation:
+            logging.info(
+                f"Received secret {event.secret.label} but couldn't parse, seems irrelevant"
+            )
+            return
+
+        if relation.app != self.charm.app:
+            logging.info("Secret removed event ignored for non Secret Owner")
+            return
+
+        if relation.name != self.relation_name:
+            logging.info("Secret changed on wrong relation.")
+            return
+
+        event.remove_revision()
 
     @abstractmethod
     def _handle_event(
@@ -2183,7 +2208,13 @@ class ResourceProviderEventHandler(EventHandlers, Generic[TRequirerCommonModel])
         if not isinstance(event, RelationChangedEvent):
             return
 
-        for key in ["entity-type", "extra-user-roles", "extra-group-roles"]:
+        for key in [
+            "resource",
+            "entity-type",
+            "entity-permissions",
+            "extra-user-roles",
+            "extra-group-roles",
+        ]:
             if key in _diff.changed:
                 raise ValueError(f"Cannot change {key} after relation has already been created")
 
