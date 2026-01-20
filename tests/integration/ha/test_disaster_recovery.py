32a7c32e5e7cdf8c3c8fd45da31f2c505e51e901
Soundarya03 <soundarya1103@gmail.com>
2026-01-20 21:55:02 +0530

    fix: test_disaster_recovery.py

--- tests/integration/ha/test_disaster_recovery.py
+++ tests/integration/ha/test_disaster_recovery.py
@@ -5,6 +5,7 @@
 import logging
 import time
 
+import jubilant
 import pytest
 from jubilant import Juju, TaskError
 
@@ -153,7 +154,19 @@ def test_recover_from_majority_failure(juju_lxd_model: Juju) -> None:
                 logger.info("Waiting for a leader to be elected")
                 time.sleep(10)
 
+    juju_lxd_model.wait(
+        lambda status: does_status_match(
+            status,
+            expected_status={
+                APP_NAME: ExpectedStatus(
+                    unit_status=[ClusterStatuses.CLUSTER_FAILED.value], unit_count=2
+                )
+            },
+        )
+        and jubilant.all_agents_idle(status, APP_NAME)
+    )
     logger.info("Rebuilding cluster after majority failure")
+    logger.info(f"Status rn: {juju_lxd_model.status()}")
     rebuild_response = juju_lxd_model.run(leader_unit, "rebuild-cluster")
     assert rebuild_response.return_code == 0, "rebuild failed"
 
