f5869fa9f84d6569c8e1379fb1b7a52ccebd7e1f
reneradoi <rene.radoi@canonical.com>
2026-05-21 12:28:30 +0200

    Revert "revert additional sleeps for refresh"
    This reverts commit 2a8335cc3aa0772e69e2d4b80f87f26d2f5df7a1.

--- tests/integration/ha/upgrades/test_upgrade_and_rollback.py
+++ tests/integration/ha/upgrades/test_upgrade_and_rollback.py
@@ -4,6 +4,7 @@
 
 import logging
 from platform import machine
+from time import sleep
 
 from jubilant import Juju
 
@@ -77,6 +78,8 @@ def test_fail_upgrade_and_rollback(charm: str, juju_vm_model: Juju) -> None:
 
     logger.info(f"Pause etcd service on unit {refresh_order[-1]} to force upgrade to fail")
     disable_etcd_service(juju_vm_model, unit_name=refresh_order[-1])
+    logger.info("Wait for the refresh to initiate")
+    sleep(90)
 
     # versions will always be marked "incompatible" if refresh to a local version
     # see: https://github.com/canonical/charm-refresh/blob/main/charm_refresh/_main.py#L182-L185
@@ -194,6 +197,8 @@ def test_upgrade_to_local(charm: str, juju_vm_model: Juju) -> None:
     # initiate the upgrade
     logger.info(f"Refresh etcd to v{WORKLOAD_VERSION['target']}")
     juju_vm_model.refresh(app=APP_NAME, path=charm)
+    logger.info("Wait for the refresh to initiate")
+    sleep(90)
 
     # versions will always be marked "incompatible" if refresh to a local version
     # this will not be the case when the PR is released
