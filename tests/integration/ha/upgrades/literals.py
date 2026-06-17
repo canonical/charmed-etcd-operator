1a707c65b8b1e6603358176738212107af77edd4
reneradoi <rene.radoi@canonical.com>
2026-06-17 13:07:46 +0200

    update etcd to 3.6.12

--- tests/integration/ha/upgrades/literals.py
+++ tests/integration/ha/upgrades/literals.py
@@ -6,6 +6,6 @@
 
 NUM_UNITS = 3
 CHARM_CHANNEL = "3.6/edge"
-CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 163, "aarch64": 164}
-WORKLOAD_VERSION = {"previous": "3.6.10", "target": "3.6.11"}
+CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 172, "aarch64": 171}
+WORKLOAD_VERSION = {"previous": "3.6.11", "target": "3.6.12"}
 CERTIFICATE_EXPIRY_TIME = 250
