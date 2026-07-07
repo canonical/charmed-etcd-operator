096734608f6f64839827c385a731036be464b98d
Andreia <andreia.velasco@canonical.com>
2026-07-07 13:04:26 +0200

    Update default branch in conf.py
    Signed-off-by: Andreia <andreia.velasco@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -136,7 +136,7 @@ html_context = {
     # Docs branch in the repo; used in links for viewing the source files
     #
     # TODO: To customise the branch, uncomment and update as needed.
-    'repo_default_branch': '3.6/docs-url-migration',
+    'repo_default_branch': '3.6/edge',
     # Docs location in the repo; used in links for viewing the source files
     #
 
