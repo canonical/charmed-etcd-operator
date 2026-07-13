78d868857d2c4f29d05a91dcf60e3fe4ab1f580e
René Radoi <rene.radoi@canonical.com>
2026-07-13 12:11:26 +0200

    update the etcd workload version to 3.6.13 (#245)

--- tests/integration/ha/upgrades/literals.py
+++ tests/integration/ha/upgrades/literals.py
@@ -6,6 +6,6 @@
 
 NUM_UNITS = 3
 CHARM_CHANNEL = "3.6/edge"
-CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 172, "aarch64": 171}
-WORKLOAD_VERSION = {"previous": "3.6.11", "target": "3.6.12"}
+CHARM_REVISIONS_TO_DEPLOY = {"x86_64": 177, "aarch64": 178}
+WORKLOAD_VERSION = {"previous": "3.6.12", "target": "3.6.13"}
 CERTIFICATE_EXPIRY_TIME = 250
